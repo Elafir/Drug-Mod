7ea9c19ef60be3b7a5c971da26ffa4de673ff230
Elafir <51381800+Elafir@users.noreply.github.com>
2022-05-20 22:12:22 -0700

    added coca

---
+++ src/main/java/deez/nuts/drugs/item/CocaleafItem.java
@@ -0,0 +1,53 @@
+
+package deez.nuts.drugs.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.block.BlockState;
+
+import deez.nuts.drugs.DrugsModElements;
+
+@DrugsModElements.ModElement.Tag
+public class CocaleafItem extends DrugsModElements.ModElement {
+	@ObjectHolder("drugs:cocaleaf")
+	public static final Item block = null;
+
+	public CocaleafItem(DrugsModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(3).saturation(0.3f)
+
+							.build()));
+			setRegistryName("cocaleaf");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 32;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
